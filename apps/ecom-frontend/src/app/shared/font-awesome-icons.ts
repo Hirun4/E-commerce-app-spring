fa3797c228f80f4d7b323019f2ea3dce711dabc0
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2025-01-12 16:21:32 +0530

    changes

--- apps/ecom-frontend/src/app/shared/font-awesome-icons.ts
+++ apps/ecom-frontend/src/app/shared/font-awesome-icons.ts
@@ -1,9 +1,9 @@
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
-import { faUser as faUserRegular } from '@fortawesome/free-regular-svg-icons'; // Regular user icon
-import { faUser as faUserSolid, faHome } from '@fortawesome/free-solid-svg-icons'; // Solid icons
+// import { faUser as faUserRegular } from '@fortawesome/free-regular-svg-icons'; // Regular user icon
+import { faUser , faHome } from '@fortawesome/free-solid-svg-icons'; // Solid icons
 
 export const fontAwesomeIcons: IconDefinition[] = [
-  faUserRegular, // Regular style
-  faUserSolid,   // Solid style
+//   faUserRegular, // Regular style
+  faUser,   // Solid style
   faHome,        // Additional solid icon
 ];
