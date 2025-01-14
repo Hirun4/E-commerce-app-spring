fe10997d71c534a94e350c45d9a8ed0fc6d96642
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2025-01-14 10:45:00 +0530

    changes

---
+++ apps/ecom-backend/src/test/java/fr/boys/ecom/EcomApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.boys.ecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
